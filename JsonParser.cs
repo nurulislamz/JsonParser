b662cce1b2a8d227392ee3d5d65c50b420ac0947
CORP\m_islam <mohammednurul.islam@tpicap.com>
2025-01-28 13:41:58 +0000

    First commit

---
+++ JsonParser.cs
@@ -0,0 +1,34 @@
+namespace JsonParser
+{
+    public class JsonParser
+    {
+        private string fileDir;
+        private string fileContents;
+        private Stack<char> bracketTrack = new Stack<char>();
+        public JsonParser(string fileDir)
+        {
+            this.fileDir = fileDir;
+            this.fileContents = File.ReadAllText(fileDir);
+        }
+
+        public bool ValidateJson()
+        {
+            for (int i = 0; i < this.fileContents.Length; i++)
+            {
+                if (this.fileContents[i] == '{')
+                {
+                    bracketTrack.Push(this.fileContents[i]);
+                }
+                else if (this.fileContents[i] == '}' && bracketTrack.TryPeek(out var topOfStack) && topOfStack == '{')
+                {
+                    bracketTrack.Pop();
+                }
+            }
+
+            if (bracketTrack.Count > 0) { return false; }
+            else { return true; };
+        }
+
+        public bool ValidateKeyValuePairs() { }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+
+namespace JsonParser
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            if (args.Length == 0) 
+            {
+                Console.WriteLine("No file parsed.");
+                return 2;
+            }
+            Console.WriteLine($"Looking for file: {args[0]}");
+            
+            string filePath = args[0];
+            JsonParser parseJson = new JsonParser(args[0]);
+
+            if (parseJson.ValidateJson()) Console.WriteLine("valid Json");
+
+            return 0;
+        }
+
+    }
+}
