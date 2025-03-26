3b1192e2f63ddefd99b14e50aee5925ea963e24a
Nurul <nurulislamz2600@gmail.com>
2025-03-26 00:04:57 +0000

    record instead of class

--- JsonTokens.cs
+++ JsonTokens.cs
@@ -6,17 +6,5 @@ using System.Threading.Tasks;
 
 namespace JsonParser
 {
-    public class JsonToken
-    {
-        public JsonTokenType Type { get; set; }
-        public string Value { get; set; }
-        public int Position { get; set; }
-
-        public JsonToken(JsonTokenType type, string value, int position)
-        {
-            this.Type = type;
-            this.Value = value;
-            this.Position = position;
-        }
-    }
+    public record JsonToken(JsonTokenType Type, string Value, int Position);
 }
